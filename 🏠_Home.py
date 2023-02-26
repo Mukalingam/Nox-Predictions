e9e901c2000749d87bbfb5860da53907b5e74110
Mukalingam <86455215+Mukalingam@users.noreply.github.com>
2023-02-26 12:11:49 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -39,6 +39,14 @@ right: 2rem;
 }}
 </style>
 """
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 thedate = datetime.date.today()
 def run():
